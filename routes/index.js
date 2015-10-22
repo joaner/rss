4097526dbcca9c4e5776524ac4296b047c491fff
joaner <joaner1206@gmail.com>
2015-10-22 15:12:43 +0800

    add 添加基础的订阅更新页面

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET feed list page. */
+router.get('/feed', function(req, res, next) {
+  var redis = require("lib/redis").client;
+  var last = req.last || 10000000000000, limit = 10;
+
+  redis.zrevrangebyscore(["rss:site:0", last, 0, "LIMIT", 0, limit], function(err, feeds){
+	if (err) throw err;
+
+	for (var k in feeds) {
+		feeds[k] = JSON.parse(feeds[k]);
+	}
+	res.render('feed', { title: 'Feed', feeds: feeds });
+  });
+});
+
+
 module.exports = router;
